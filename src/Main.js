aa245e26f6ab31b50f687bdf4338a2d60b0d8291
batya <batya.yel@gmail.com>
2022-07-11 14:18:51 +0300

    pull 14:15

--- src/Main.js
+++ src/Main.js
@@ -1,9 +1,5 @@
 import { Route, Routes } from 'react-router-dom'
 import Instructions from './pages/Freestyle/Instructions'
-<<<<<<< HEAD
-
-=======
->>>>>>> 12813c1f489076427d3b27a28cdb5b579098a813
 export default function Main() {
 
     return <>
