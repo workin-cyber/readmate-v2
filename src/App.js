9682a156d10d91fe8f87a54d1c6c8d3fbd84b3cd
yosefus <yosalsoni@gmail.com>
2022-09-01 10:34:56 +0300

    css changes - prettier

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 // import TestStartFooter from "./TestStartFooter";
 // import TestExercise from "./TestExercise";
 import { BrowserRouter } from "react-router-dom";
-import "./App.css";
 import { DataProvider } from "./context/manageContext";
 import Layout from "./Layout";
 // import Test from "./Test";
--- src/components/Common/TitleRead/index.js
+++ src/components/Common/TitleRead/index.js
@@ -1,14 +1,13 @@
 // Creator : Team A - Daniel & Yehonatan
-  import './style.css';
+import './style.css';
 /**
  * 
  * @param {{color:string}} props 
  * @returns 
  */
-export default function TitleRead (props) 
-{
-return <div className="title-read" style={{color:props.color}}> 
-{/* title  */}
-Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla dolor quibusdam cum?
-</div>
+export default function TitleRead(props) {
+  return <div className="title-read" style={{ color: props.color }}>
+    {/* title  */}
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla dolor quibusdam cum?
+  </div>
 }
